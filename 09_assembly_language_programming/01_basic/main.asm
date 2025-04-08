80b077309625568aee3be6d6915ba5e7f329d361
harveyliao <harvey.liao@mail.utoronto.ca>
2025-04-08 14:05:52 -0400

    assembly, 01_basic, add comments

--- 09_assembly_language_programming/01_basic/main.asm
+++ 09_assembly_language_programming/01_basic/main.asm
@@ -8,15 +8,16 @@ section .text
 main:
   ; printf("Hello, world!\n");
   ; cout << "Hello, world!" << endl;
-  mov rdi, message
-  mov rax, 0
-  push rbx
-  call printf
-  pop rbx
+  mov rdi, message  ; rdi is the first argument of printf
+  mov rax, 0        ; store the return value of function
+  push rbx          ; push rbx onto the stack
+                    ; this is to adjust the stack pointer to be a multiple of 8
+  call printf       ; call printf
+  pop rbx           ; pop rbx from the stack, clean up
 
   ; exit
-  mov rax, 0
-  ret
+  mov rax, 0        ; rax is the return value of the program
+  ret               ; return from the function
 
 section .data
   message db "Hello, world!", 0ah, 0dh, 0
