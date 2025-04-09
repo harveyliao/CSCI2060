9ceb7b3ce6a63974431b67b02209e5437603b399
harveyliao <harvey.liao@mail.utoronto.ca>
2025-04-09 13:13:06 -0400

    add comments

--- 09_assembly_language_programming/06_uppercase/main.asm
+++ 09_assembly_language_programming/06_uppercase/main.asm
@@ -14,6 +14,7 @@ main:
   pop rax 
 
   ; read the message from the user
+  ; scanf will put a null terminator at the end of the string
   mov rdi, messageFormat
   mov rsi, message
   mov rax, 0
@@ -26,23 +27,31 @@ main:
   mov rsi, message 
 
 nextChar:
-  mov al, [rsi]
+  
+  mov al, [rsi]    ; al is the low bits of RAX, it will contain the current character
 
-  cmp al, 0
-  je printResult 
+  cmp al, 0        ; compare the current character to the null terminator
+  je printResult   ; if we've reached the end of the string, print the result
 
+  ; check if the character is a lowercase letter
+        ; look up the ASCII table.
+        ; 97 is the ASCII code for 'a'
+        ; 122 is the ASCII code for 'z'
   cmp al, 97
   jl skipChar 
 
   cmp al, 122 
   jg skipChar 
 
-  sub al, 32
-  mov [rsi], al
+  ; convert the character to uppercase
+  ; 32 is the difference between the ASCII codes of lowercase and uppercase letters
+  sub al, 32          ; subtract 32 from the character to convert it to uppercase
+  mov [rsi], al       ; store the uppercase character back in the message
 
 skipChar:
-  inc rsi 
-  jmp nextChar
+  inc rsi             ; move to the next character
+                      ; increment by 1 because that's the size of a character
+  jmp nextChar        ; repeat the loop
 
 printResult:
   mov rdi, result
